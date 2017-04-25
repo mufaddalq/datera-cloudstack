3a772058007732759c670f72edc7526a829630b0
muffadal <muffadalq@gmail.com>
2017-04-25 14:51:10 -0700

    Adding exception to handle selecting cluster-wide scope cleanly

--- plugins/storage/volume/datera/src/org/apache/cloudstack/storage/datastore/lifecycle/DateraPrimaryDataStoreLifeCycle.java
+++ plugins/storage/volume/datera/src/org/apache/cloudstack/storage/datastore/lifecycle/DateraPrimaryDataStoreLifeCycle.java
@@ -179,7 +179,8 @@ public class DateraPrimaryDataStoreLifeCycle implements PrimaryDataStoreLifeCycl
 
     @Override
     public boolean attachCluster(DataStore store, ClusterScope scope) {
-        return true; // should be ignored for zone-wide-only plug-ins
+        throw new UnsupportedOperationException("Only Zone-wide scope is supported with the Datera Storage driver");
+        //return true; // should be ignored for zone-wide-only plug-ins
     }
 
     @Override
